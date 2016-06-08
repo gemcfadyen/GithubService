10ddc488a9fb4f1ad11f202f22790a9077beca0d
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-06-08 13:02:58 +0100

    Queries for the languages of a particular repository

--- test/github/client_test.exs
+++ test/github/client_test.exs
@@ -2,9 +2,20 @@ defmodule GithubService.Github.ClientTest do
   use ExUnit.Case
   alias GithubService.Github.Client
 
-  test "queries for user" do
+  @tag :integration
+  test "gets the repositories for a user" do
     response = Client.get_repositories_for("hackeryou")
 
     assert response =~ "languages_url"
+  end
+
+  @tag :integration
+  test "gets the languages for a particular repository" do
+    response = Client.get_languages_for("hackeryou", "amazon")
+
+    assert response =~ "Ruby"
+    assert response =~ "CSS"
+    assert response =~ "JavaScript"
+    assert response =~ "CoffeeScript"
   end
 end
--- web/github/client.ex
+++ web/github/client.ex
@@ -1,9 +1,17 @@
 defmodule GithubService.Github.Client do
+  @root_url "https://api.github.com/"
 
   def get_repositories_for(user) do
-    url = "https://api.github.com/users/#{user}/repos"
-    response = HTTPotion.get(url, headers: ["User-Agent": "Github Service App"])
-    response.body
+    url = @root_url <> "users/#{user}/repos"
+    get(url).body
   end
 
+  def get_languages_for(user, repository) do
+    url = @root_url <> "repos/#{user}/#{repository}/languages"
+    get(url).body
+  end
+
+  defp get(url) do
+    HTTPotion.get(url, headers: ["User-Agent": "Github Service App"])
+  end
 end
